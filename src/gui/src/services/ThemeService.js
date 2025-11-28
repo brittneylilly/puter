176462b61e9ddb7b0548ee492e1ace65a472ae68
Brittney Lilly <185453450+brittneylilly@users.noreply.github.com>
2025-11-27 21:37:51 -0500

    Dynamically change sidebar text color to adapt to lightness adjustments

--- src/gui/src/services/ThemeService.js
+++ src/gui/src/services/ThemeService.js
@@ -121,6 +121,8 @@ export class ThemeService extends Service {
         this.root.style.setProperty('--primary-lightness', s.lig + '%');
         this.root.style.setProperty('--primary-alpha', s.alpha);
         this.root.style.setProperty('--primary-color', s.light_text ? 'white' : '#373e44');
+        // window sidebar title color
+        this.root.style.setProperty('--window-sidebar-title-color', s.lig <= 50.00 ? 'white' : 'black');
 
         // TODO: Should we debounce this to reduce traffic?
         this.#broadcastService.sendBroadcast('themeChanged', {
